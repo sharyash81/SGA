aed6d63d5209cfca79a788572e15ae6534046531
root <root@pop-os.localdomain>
2023-01-13 15:00:39 +0330

    web scraper added

--- sga_prep/json2mongo.sh
+++ sga_prep/json2mongo.sh
@@ -6,10 +6,13 @@ echo "***** start mongodb service *****"
 systemctl start mongod
 ./sqlite2Json.sh "database.sqlite"
 python3 data_cleaner.py
+rm -rf 'jsonCols/Team_Attributes.json'
+rm -rf 'jsonCols/League.json'
+python3 web_scraper.py
 
 echo "***** DB Creation process starts *****"
-rm -rf 'jsonCols/Team_Attributes.json'
 mongosh --eval "use '${db_name}'"
 for clc in $( ls jsonCols);do
 	mongoimport --db $db_name --collection ${clc%.*} --file ./jsonCols/$clc --jsonArray
 done
+
